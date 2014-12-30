f200d74a153594a642ee948fd44797fbf55fb416
zhanghao <zhanghao@pinganfang.com>
2014-12-30 14:18:30 +0800

    commit

--- feather_conf.js
+++ feather_conf.js
@@ -2,8 +2,6 @@ feather.config.merge({
 	staticMode: true,	//开启静态资源内联模式
 	autoCombine: true, 	//开启静态资源内联模式后，使用autocombine可自动合并页面上的零散资源
 
-	namespace: 'abc',
-
 	md5Query: {
 		open: true,
 		name: 'version'
