7569c895c4621ec290ff408c6ba119038876c288
alienyu <alienyu@foxmail.com>
2015-01-15 16:33:47 +0800

    add

--- static/js/mod/tabs/tabs.js
+++ static/js/mod/tabs/tabs.js
@@ -1,4 +1,4 @@
-var $ = require('jquery');
+var $ = require(':jquery');
 
 var tabs = function(opt){
     this.options = $.extend({
--- static/js/ui.js
+++ static/js/ui.js
@@ -2,10 +2,14 @@ require.async([':jquery', ':highlighter'], function($, Highligter){
 	$(document).ajaxStop(function(){
 		$('script[type="text/html"]', this).each(function(){
 			var text = this.text;
-
-			$('<a href="javascript:void(0);" class="run-btn">').text('点击运行').insertBefore(this).click(function(){
+			if($(this).attr("data-type") == "noclick") {
 				(new Function(text)).call(window);
-			});
+			} else if ($(this).attr("data-type") != "noclick" && $(this).attr("data-type") != "show"){
+				$('<a href="javascript:void(0);" class="run-btn">').text('点击运行').insertBefore(this).click(function(){
+					(new Function(text)).call(window);
+				});
+			}
+
 
 			var $pre = $('<pre class="code">').text(text).insertBefore(this);
 
